d1bf714bc37894282298cf7926d8bd951d5e6c69
Hrustch <nik.stalker47@gmail.com>
2023-04-27 14:30:06 +0300

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
+const galleryRender = document.querySelector(".gallery")
 // Change code below this line
+galleryItems.forEach(el=>{
+    galleryRender.insertAdjacentHTML("beforeend", `<li class="gallery__item">
+    <a class="gallery__link" href="${el.original}">
+      <img class="gallery__image" src="${el.preview}" data-source="${el.original}" alt="${el.description}" />
+    </a>
+  </li>`)
+})
+function fullscreenImageOpener(event){
+  event.preventDefault()
+if(event.target.nodeName !== "IMG"){
+  return
+}
+const selectedImage = event.target.dataset.source;
 
-console.log(galleryItems);
+const fullscreenImage = basicLightbox.create(`<img src="${selectedImage}">`)
+fullscreenImage.show()
+function escapeHandler(e){
+  if(e.key === "Escape") {
+    fullscreenImage.close();
+    document.removeEventListener('keydown', escapeHandler);
+  }
+}
+galleryRender.addEventListener("keydown", escapeHandler);
+}
+galleryRender.addEventListener("click", fullscreenImageOpener)
